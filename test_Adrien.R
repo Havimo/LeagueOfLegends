d01a976774e184e0b05e3593b6beca1c7fb83d3d
de Castro <adecastro@ea.com>
2017-11-29 14:23:08 +0100

    added sample and some reg subsets

--- test_Adrien.R
+++ test_Adrien.R
@@ -36,6 +36,17 @@ plot(reg.backward)
 plot(reg.forward)
 
 
+#regsubsets for gold
+reg.exhaustive <- regsubsets(x=goldearned~.,data=teams.new.dt[,!c('win','deaths','goldspent','firstinhib','inhibkills','towerkills','firstbaron','firstdragon','matchid','teamid')]
+                             ,really.big = T,method = 'exhaustive')
+reg.backward <- regsubsets(x=firstinhib~.,data=teams.new.dt[,!c('matchid','teamid','win')],really.big = T,method = 'backward')
+reg.forward <- regsubsets(x=firstinhib~.,data=teams.new.dt[,!c('matchid','teamid','win')],really.big = T,method = 'forward')
+
+plot(reg.exhaustive)
+plot(reg.backward)
+plot(reg.forward)
+
+
 #Plot test
 # error.dt <- as.data.table(cbind(1:nrow(reg.summary$which),reg.summary$rsq,reg.summary$rss,reg.summary$adjr2,reg.summary$cp,reg.summary$bic))
 # setnames(error.dt,c('model','rsq','rss','adjr2','cp','bic'))
@@ -51,11 +62,21 @@ glmnet.model <- glmnet(x = matrix.input, y = matrix.response, family = 'binomial
 
 NormalizedGoldEarnedBoxPlot(teams.dt,teams.normalized.dt)
 
+#scaling : 
+
+teams.scaled.dt <- as.data.table(sapply(teams.new.dt,scale,2))
+teams.scaled.dt$win <- teams.new.dt$win #reassign win
+
 
 #base model to test variables interaction and behavior
 test.model <- glm(teams.new.dt[,.(win,deaths,goldearned)],formula= win~deaths+goldearned+I(deaths^2)+I(goldearned^2),family='binomial')
 test.model <- lm(teams.new.dt[,!c('win','firstinhib','inhibkills','towerkills','firstbaron','firstdragon','matchid','teamid')],formula= deaths~.)
 
 #plotting
-ClusteringPlot(teams.new.dt[sample(1:nrow(teams.new.dt),200000)],'deaths','goldearned')
+ClusteringPlot(teams.new.dt,'deaths','goldearned')
+ClusteringPlot(teams.dt,'deaths','goldearned')
 CompleteTeamModel_kCV(teams.new.dt,k=10,formula=' win~deaths+goldearned+I(deaths^2)+I(goldearned^2)')
+CompleteTeamModel_kCV(teams.new.dt,k=10,formula=' win~deaths+goldearned')
+CompleteTeamModel_kCV(teams.dt,k=10,formula=' win~deaths+goldearned')
+CompleteTeamModel_kCV(teams.new.dt,k=10,formula=' win~deaths+kills+totminionskilled')
+CompleteTeamModel_kCV(teams.scaled.dt,k=10,formula=' win~deaths+kills+totminionskilled')
